da08ca2254417df5515e0f149030f0c3bcec1679
Denis Popkov <popkov-den03@mail.ru>
2024-03-10 12:10:41 +0600

    add: login method logic

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,21 +4,45 @@ import (
 	"context"
 	ssov1 "github.com/DenisPopkov/protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		phoneNumber string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		phoneNumber string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
+
+	token, err := s.auth.Login(ctx, req.GetPhoneNumber(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
 	return &ssov1.LoginResponse{
-		Token: req.GetPhoneNumber(),
+		Token: token,
 	}, nil
 }
 
